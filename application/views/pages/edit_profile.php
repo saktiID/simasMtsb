8287be787497861ee4112ea5e15703b70e3140a1
SaktiID <51077353+saktiID@users.noreply.github.com>
2021-08-06 17:03:12 +0700

    edit view

--- application/views/pages/edit_profile.php
+++ application/views/pages/edit_profile.php
@@ -18,7 +18,7 @@
                 <div class="d-flex align-items-end flex-wrap">
                     <div class="d-flex">
                         <i class="mdi mdi-home text-muted hover-cursor"></i>
-                        <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Data Guru&nbsp;/&nbsp;Edit Profile&nbsp;/&nbsp;</p>
+                        <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp; <a href="<?= base_url('dataguru'); ?>">Data Guru</a>&nbsp;/&nbsp;Edit Profile&nbsp;/&nbsp;</p>
                     </div>
                 </div>
             </div>
