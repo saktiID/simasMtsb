38b9f597995d12265a526a8f2ea65bae8bdd9bfc
SaktiID <51077353+saktiID@users.noreply.github.com>
2022-02-02 11:18:02 +0700

    add endpoint guru

--- application/controllers/API_Raport.php
+++ application/controllers/API_Raport.php
@@ -11,8 +11,16 @@ class API_Raport extends RestController
         $this->load->model('RaportDB_model', 'api_raport');
     }
 
+    /*
+    | -----------------------------------------------
+    | END POINT FOR SISWA
+    | -----------------------------------------------
+    */
+
     /**
-     * method controller for get siswa spesifik by siswa is or all siswa
+     * method controller for get siswa spesifik by siswa id or all siswa
+     * @param string $opt
+     * @param string $siswa_id
      */
     public function siswa_get()
     {
@@ -38,7 +46,7 @@ class API_Raport extends RestController
                         $this->response([
                             'status' => false,
                             'message' => 'parameter siswa_id dibutuhkan.'
-                        ], 404);
+                        ], 400);
                     }
                     break;
                 case 'all_siswa':
@@ -69,8 +77,16 @@ class API_Raport extends RestController
         }
     }
 
+    /*
+    | -----------------------------------------------
+    | END POINT FOR KELAS
+    | -----------------------------------------------
+    */
+
     /**
-     * method controller for get siswa kelas by kelas id
+     * method controller for get siswa kelas or info kelas by kelas id
+     * @param string $opt
+     * @param string $kelas_id
      */
     public function kelas_get()
     {
@@ -130,6 +146,101 @@ class API_Raport extends RestController
             $this->response([
                 'status' => false,
                 'message' => 'parameter opt dibutuhkan. opt: info_kelas|siswa_kelas'
+            ], 400);
+        }
+    }
+
+    /*
+    | -----------------------------------------------
+    | END POINT FOR GURU
+    | -----------------------------------------------
+    */
+
+    /**
+     * method controller for get specific guru or all guru by guru id
+     * @param string $opt
+     * @param string $guru_id
+     */
+    public function guru_get()
+    {
+        $opt = $this->get('opt');
+        $guru_id = $this->get('guru_id');
+        if ($opt) {
+            switch ($opt) {
+                case 'specific_guru':
+                    if ($guru_id) {
+                        $guru = $this->api_raport->get_guru($guru_id);
+                        if ($guru) {
+                            $this->response([
+                                'status' => true,
+                                'message' => $guru
+                            ], 200);
+                        } else {
+                            $this->response([
+                                'status' => false,
+                                'message' => 'guru dengan guru_id ' . $guru_id . ' tidak ditemukan.'
+                            ], 404);
+                        }
+                    } else {
+                        $this->response([
+                            'status' => false,
+                            'message' => 'parameter guru_id dibutuhkan.'
+                        ], 400);
+                    }
+                    break;
+                case 'all_guru':
+                    $guru = $this->api_raport->get_all_guru();
+                    if ($guru) {
+                        $this->response([
+                            'status' => true,
+                            'message' => $guru
+                        ], 200);
+                    } else {
+                        $this->response([
+                            'status' => false,
+                            'message' => 'tidak ada guru ditemukan'
+                        ], 404);
+                    }
+                    break;
+                case 'walas_guru':
+                    if ($guru_id) {
+                        $guru = $this->api_raport->get_guru($guru_id);
+                        if ($guru) {
+                            $walas = $this->api_raport->get_walas($guru_id);
+                            if ($walas) {
+                                $this->response([
+                                    'status' => true,
+                                    'message' => $walas
+                                ], 200);
+                            } else {
+                                $this->response([
+                                    'status' => false,
+                                    'message' => 'guru dengan guru_id ' . $guru_id . ' bukan sebagai walas.'
+                                ], 404);
+                            }
+                        } else {
+                            $this->response([
+                                'status' => false,
+                                'message' => 'guru dengan guru_id ' . $guru_id . ' tidak ditemukan.'
+                            ], 404);
+                        }
+                    } else {
+                        $this->response([
+                            'status' => false,
+                            'message' => 'parameter guru_id dibutuhkan.'
+                        ], 400);
+                    }
+                    break;
+                default:
+                    $this->response([
+                        'status' => false,
+                        'message' => 'value opt tidak valid. opt: specific_guru|all_guru|walas_guru'
+                    ], 400);
+            }
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'parameter opt dibutuhkan. opt: specific_guru|all_guru|walas_guru'
             ], 400);
         }
     }
