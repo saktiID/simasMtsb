19cd031ceca38e3dca6bc6c97d46b5136f96de82
SaktiID <51077353+saktiID@users.noreply.github.com>
2023-01-04 13:23:18 +0700

    tes view

--- application/controllers/WebExcel.php
+++ application/controllers/WebExcel.php
@@ -26,11 +26,11 @@ class WebExcel extends CI_Controller
                     $level = $_GET['level'];
                     $jenjang = $_GET['jenjang'];
 
-                    $jenjang = $this->nilaiEci_model->get_id_kelas_by_name($jenjang)[0]['id'];
-                    $siswa = $this->nilaiEci_model->get_level_in_grade($level, $jenjang);
+                    // $jenjang = $this->nilaiEci_model->get_id_kelas_by_name($jenjang)[0]['id'];
+                    // $siswa = $this->nilaiEci_model->get_level_in_grade($level, $jenjang);
 
                     $data = [
-                        'siswa' => $$this->nilaiEci_model(),
+                        'siswa' => $this->nilaiEci_model->get_id_kelas_by_name('VIII'),
                         'level' => $level,
                     ];
                     $this->load->view('webexcel/fetchExcel', $data);
