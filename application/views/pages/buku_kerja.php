068c5fffcfcd4d4f0e7290dde4595a79e94c69c9
SaktiID <51077353+saktiID@users.noreply.github.com>
2023-07-03 22:24:11 +0700

    fix bug tahun ajar

--- application/views/pages/buku_kerja.php
+++ application/views/pages/buku_kerja.php
@@ -128,7 +128,8 @@
                                 <option selected value="">-- Tahun ajaran --</option>
                                 <option value="2021-2022">2021 - 2022</option>
                                 <option value="2022-2023">2022 - 2023</option>
-                                <option value="2024-2024">2023 - 2024</option>
+                                <option value="2023-2024">2023 - 2024</option>
+                                <option value="2024-2025">2023 - 2024</option>
                             </select>
                         </div>
                         <!-- input semester -->
