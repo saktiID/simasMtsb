c510f4791f819b970aae92b152fd0f66637e692e
SaktiID <51077353+saktiID@users.noreply.github.com>
2023-07-03 22:11:28 +0700

    fix error print eci from qr

--- application/controllers/QNilaiEci.php
+++ application/controllers/QNilaiEci.php
@@ -121,6 +121,7 @@ class QNilaiEci extends CI_Controller
             'describe_vocab' => $data_eci[0]['describe_vocab'],
             'timestamp' => $data_eci[0]['timestamp'],
             'link' => $data_eci[0]['link'],
+            'level' => $data_siswa[0]['level_eci'],
         ];
         $this->load->view('print_pdf/print_nilai_eci2', $data);
     }
